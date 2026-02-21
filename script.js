bd0fb33def03e024d8d2b52a528ac50b6e39a24a
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2026-02-21 08:46:46 -0300

    :sparkles: feat: displaying user account movements

--- script.js
+++ script.js
@@ -68,3 +68,17 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+       <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i} ${type}</div>
+        <div class="movements__value">${mov} €</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
