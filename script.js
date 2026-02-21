854dd8d76118779600250e9a22378dbdfc4c1379
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2026-02-21 08:52:48 -0300

    :sparkles: feat: displaying the calculation of movements on the screen

--- script.js
+++ script.js
@@ -69,16 +69,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+// Displaying user account movements
 const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
        <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i} ${type}</div>
-        <div class="movements__value">${mov} €</div>
+        <div class="movements__value">${mov}€</div>
       </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (mov) {
+  const balance = mov.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
